cbcd029591c646d66d2647719b93baec953d7296
Anthony Akpan <anthony.akpan@hotmail.com>
2023-08-22 13:06:44 -0700

    clean up env variable

--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const ApiClient = Axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_ENDPOINT}/api/`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_ENDPOINT ?? ''}/api/`,
   headers: {
     "Content-Type": "application/json",
     "X-Requested-With": "XMLHttpRequest",
