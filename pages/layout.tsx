4207b8cd469c640fc2bd6b89f5c06e7dbba51ada
Anthony Akpan <anthony.akpan@hotmail.com>
2023-08-22 12:43:51 -0700

    remove text

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,17 +1,15 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        HEYYYY
-        {children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
