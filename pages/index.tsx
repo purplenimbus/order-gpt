3dac3850ae3ae01b0b18ee3463abb4b2e5397abf
Anthony Akpan <anthony.akpan@hotmail.com>
2023-08-23 13:49:30 -0700

    Fix build errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <main className="flex h-full min-h-screen flex-col items-center justify-between bg-white">
-      <VoiceSlides />}
+      <VoiceSlides />
     </main>
   )
 }
